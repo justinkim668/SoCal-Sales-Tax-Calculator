0d6a990571317333f403159d19ddccbe52b5c10f
justinkim668 <justinkim668@gmail.com>
2017-07-26 00:08:52 -0400

    Initial Commit

---
+++ SoCal Sales Tax CalculatorTests/SoCal_Sales_Tax_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  SoCal_Sales_Tax_CalculatorTests.swift
+//  SoCal Sales Tax CalculatorTests
+//
+//  Created by Justin Kim on 7/26/17.
+//  Copyright © 2017 Justin Kim. All rights reserved.
+//
+
+import XCTest
+@testable import SoCal_Sales_Tax_Calculator
+
+class SoCal_Sales_Tax_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
